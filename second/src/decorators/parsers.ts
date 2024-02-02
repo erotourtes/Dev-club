68939e9853c64780be7c664b91311c41eb0581e9
erotourtes <erotourtes@gmail.com>
2024-02-02 08:22:50 +0200

    Fix: second writing to socket

--- second/src/decorators/parsers.ts
+++ second/src/decorators/parsers.ts
@@ -12,6 +12,8 @@ const wrapped = (handler: any, contentType: string) =>
   async function (req: Req, res: Res, ...args: any) {
     let result = await handler(req, res, ...args);
 
+    if (res.headersSent) return;
+
     res.setHeader("Content-Type", contentType);
     res.statusCode = 200;
     res.end(JSON.stringify(result));
